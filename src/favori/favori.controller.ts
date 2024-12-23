37144466811c4d0b3769eeebe7940c002e3834a6
verstands <rabbymbamu@gmail.com>
2024-12-23 11:50:38 +0100

    controller  favori end

--- src/favori/favori.controller.ts
+++ src/favori/favori.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { FavoriService } from './favori.service';
+import { FavoriDto } from 'src/dto/favori.dto';
 
 @Controller('favori')
-export class FavoriController {}
+export class FavoriController {
+    constructor(private readonly allservice: FavoriService) {}
+    
+        @Get()
+        get() {
+          return this.allservice.getAll();
+        }
+      
+        @Get(':id')
+        getFindOne(@Param('id') id: string) {
+          return this.allservice.getFind({
+            id,
+          });
+        }
+    
+        @Get('servicebyuser/:id')
+        getServiceByUser(@Param('id') id: string) {
+          return this.allservice.getServiceUser({
+            id,
+          });
+        }
+    
+      
+        @Put(':id')
+        update(@Param('id') id: string, @Body() agentUpdate: FavoriDto) {
+          return this.allservice.update({ id, ...agentUpdate });
+        }
+      
+        @Delete(':id')
+        delete(@Param('id') id: string) {
+          return this.allservice.delete({ id });
+        }
+      
+        @Post()
+        async create(@Body() agendadto: FavoriDto) {
+          return await this.allservice.create(agendadto);
+        }
+}
